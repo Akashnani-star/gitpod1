dfe5a1d144c3bfd20f18d03244244028f7c8f59c
Akashnani-star <dhanviakash1234@gmail.com>
2022-05-09 01:10:16 +0000

    initial commit

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './Header';
+import SwipeButtons from './SwipeButtons';
+import TinderCards from './TinderCards';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Header */}
+      <Header />
+      {/* Tinder Cards */}
+      <TinderCards />
+      {/* Swipe Controls */}
+      <SwipeButtons />
+    </div>
+  );
+}
+
+export default App;
